58ad396bb331a702cad058a04c7cc94fe9128190
Ashim Gautam <computer77356@gmail.com>
2024-02-23 11:04:17 +0545

    improve responsiveness in slider

--- src/Components/landingPageComponents/Carousl.jsx
+++ src/Components/landingPageComponents/Carousl.jsx
@@ -14,26 +14,27 @@ const Carousl = () => {
 
         return () => clearInterval(interval);
     }, []);
+
   return (
-    <div className=' w-full h-[30vh]'>
+    <div className=' w-full h-[30vh] md:h-[80vh]'>
         {
-            currentItem == 1  ?(
-                <div className='absolute top-[50%] right-[0%] z-10  p-3 rounded-full w-[10%] items-center text-2xl bg-slate-400' onClick={()=>setCurrentItem(currentItem+1)} >
+            currentItem === 1  ?(
+                <div className='absolute top-[50%] right-[0%] z-10  p-3 rounded-full w-[10%] md:w-[3%] items-center text-2xl bg-slate-400' onClick={()=>setCurrentItem(currentItem+1)} >
                     
                     <FaAngleRight />
                         
             </div>
-            ) : currentItem ==3 ?(
-                <div className='absolute top-[50%] z-10  p-3 rounded-full bg-slate-400 text-2xl w-[10%]'onClick={()=>setCurrentItem(currentItem-1)}>
+            ) : currentItem ===3 ?(
+                <div className='absolute top-[50%] z-10  p-3 rounded-full bg-slate-400 text-2xl w-[10%] md:w-[3%]'onClick={()=>setCurrentItem(currentItem-1)}>
                 <FaAngleLeft />
                 </div>
             ) :(
 
                 <>
-                    <div className='absolute top-[50%] z-10  p-3 rounded-full bg-slate-400 text-2xl w-[10%]'onClick={()=>setCurrentItem(currentItem-1)}>
+                    <div className='absolute top-[50%] z-10  p-3 rounded-full bg-slate-400 text-2xl w-[10%] md:w-[3%]'onClick={()=>setCurrentItem(currentItem-1)}>
                     <FaAngleLeft />
                     </div>
-                    <div className='absolute top-[50%] right-[0%] z-10  p-3 rounded-full bg-slate-400 text-2xl w-[10%]' onClick={()=>setCurrentItem(currentItem+1)}>
+                    <div className='absolute top-[50%] right-[0%] z-10  p-3 rounded-full bg-slate-400 text-2xl w-[10%] md:w-[3%]' onClick={()=>setCurrentItem(currentItem+1)}>
                         <FaAngleRight />
                     </div>
                 </>
@@ -45,14 +46,14 @@ const Carousl = () => {
             currentItem ===1 ?(
                 <div className=' flex flex-row justify-center items-center w-full h-full' >
                     <div className='w-[60%] h-full'>
-                        <div className='p-4'>
-                            <h1 className='mb-3 text-2xl font-bold tracking-wider text-blue-600 '>Find My Doctor</h1>
-                            <p className=' text-sm  w-full'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem nesciunt consectetur aut aspernatur perferendis libero ipsam laboriosam et magnam eius blanditiis numquam iste, ad repellat tempore? Adipisci, ducimus id.</p>
+                        <div className='p-4 md:p-20'>
+                            <h1 className='mb-3 md:mb-10 text-2xl md:text-6xl font-bold tracking-wider text-blue-600 '>Find My Doctor</h1>
+                            <p className=' text-sm md:text-2xl w-full md    :w-[60%]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem nesciunt consectetur aut aspernatur perferendis libero ipsam laboriosam et magnam eius blanditiis numquam iste, ad repellat tempore? Adipisci, ducimus id.</p>
                         </div>
 
-                        <div className=' ml-4 w-[80%] flex flex-row justify-between  '>
-                            <button className='w-[40%] bg-mainBg text-white text-sm font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">About Us</Link></button>
-                            <button className='w-[40%] bg-mainBg text-white text-sm font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Find Doctor</Link></button>
+                        <div className=' ml-4 md:ml-20  w-[80%] md:w-[60%] flex flex-row justify-between text-sm md:text-2xl'>
+                            <button className='w-[40%] md:pt-4 md:pb-4 bg-mainBg text-white  font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">About Us</Link></button>
+                            <button className='w-[40%] bg-mainBg text-white font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Find Doctor</Link></button>
                         </div>
                     </div> 
 
@@ -65,15 +66,15 @@ const Carousl = () => {
             ):currentItem ===2?(
                 <div className=' flex flex-row justify-center items-center w-full h-full' >
                 <div className='w-[60%] h-full'>
-                    <div className='p-4'>
-                        <h1 className='mb-3 text-2xl font-bold tracking-wider text-red-600 '>Search For Blood</h1>
-                        <p className=' text-sm  w-full'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem nesciunt consectetur aut aspernatur perferendis libero ipsam laboriosam et magnam eius blanditiis numquam iste, ad repellat tempore? Adipisci, ducimus id.</p>
-                    </div>
+                        <div className='p-4 md:p-20'>
+                            <h1 className='mb-3 md:mb-10 text-2xl md:text-6xl font-bold tracking-wider text-red-600 '>Search  Blood</h1>
+                            <p className=' text-sm md:text-2xl w-full md:w-[60%]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem nesciunt consectetur aut aspernatur perferendis libero ipsam laboriosam et magnam eius blanditiis numquam iste, ad repellat tempore? Adipisci, ducimus id.</p>
+                        </div>
                     
-                    <div className=' pl-4 w-full flex flex-row justify-evenly'>
-                        <button className='w-[40%] bg-mainBg text-white text-sm font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Find Blood</Link></button>
-                        <button className='w-[40%] bg-mainBg text-white text-sm font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Donate Blood</Link></button>
-                    </div>
+                        <div className=' ml-4 md:ml-20  w-[80%] md:w-[60%] flex flex-row justify-between text-sm md:text-2xl'>
+                            <button className='w-[40%] pt-1 pb-1 md:pt-4 md:pb-4 bg-mainBg text-white  font-semibold  rounded-sm'> <Link to="/aboutUs">Find Blood</Link></button>
+                            <button className='w-[50%] bg-mainBg text-white font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Donate Blood</Link></button>
+                        </div>
                 </div> 
 
                 <div className='w-[40%] h-full'>
@@ -85,14 +86,14 @@ const Carousl = () => {
             ):(
                 <div className=' flex flex-row justify-center items-center w-full h-full' >
                     <div className='w-[60%] h-full'>
-                        <div className='p-4'>
-                            <h1 className='mb-3 text-2xl font-bold tracking-wider text-red-600 '>Search Ambulance</h1>
-                            <p className=' text-sm  w-full'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem nesciunt consectetur aut aspernatur perferendis libero ipsam laboriosam et magnam eius blanditiis numquam iste, ad repellat tempore? Adipisci, ducimus id.</p>
+                        <div className='p-4 md:p-20'>
+                            <h1 className='mb-3 md:mb-10 text-2xl md:text-6xl font-bold tracking-wider text-red-600 '>Find Ambulance</h1>
+                            <p className=' text-sm md:text-2xl w-full md:w-[90%]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem nesciunt consectetur aut aspernatur perferendis libero ipsam laboriosam et magnam eius blanditiis numquam iste, ad repellat tempore? Adipisci, ducimus id.</p>
                         </div>
                         
-                        <div className=' pl-4 w-full flex flex-row justify-evenly'>
-                            <button className='w-[40%] bg-mainBg text-white text-sm font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Find Ambulance</Link></button>
-                            <button className='w-[40%] bg-mainBg text-white text-sm font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Register Your Ambulance</Link></button>
+                        <div className=' ml-4 md:ml-20  w-[80%] md:w-[80%] flex flex-row justify-between text-xs md:text-2xl'>
+                            <button className='w-[40%] md:pt-4 md:pb-4 bg-mainBg text-white  font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Find Ambulance</Link></button>
+                            <button className='w-[50%] bg-mainBg text-white font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Register Ambulance</Link></button>
                         </div>
                     </div> 
 
