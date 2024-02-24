f400c685800b2a592308780c61c2bb64b0fd98f6
deepakpokharel <deepakpokharel77@gmail.com>
2024-02-23 19:45:02 -0800

    navbar and carousel ui changed

--- src/Components/landingPageComponents/Carousl.jsx
+++ src/Components/landingPageComponents/Carousl.jsx
@@ -1,7 +1,8 @@
 import React, { useState,useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import ambulance from '../../images/ambulance.jpg'
-import doc1 from '../../images/doc1.jpg'
+import ambulance from '../../images/ambulance.png'
+import doc1 from '../../images/doc1.png'
+import blood from '../../images/blood.png'
 
 import { FaAngleRight,FaAngleLeft } from "react-icons/fa";
 const Carousl = () => {
@@ -52,8 +53,8 @@ const Carousl = () => {
                         </div>
 
                         <div className=' ml-4 md:ml-20  w-[80%] md:w-[60%] flex flex-row justify-between text-sm md:text-2xl'>
-                            <button className='w-[40%] md:pt-4 md:pb-4 bg-mainBg text-white  font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">About Us</Link></button>
-                            <button className='w-[40%] bg-mainBg text-white font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Find Doctor</Link></button>
+                            <button className='w-[40%] md:pt-4 md:pb-4 bg-mainBg text-white  font-medium pt-1 pb-1 rounded-2xl'> <Link to="/aboutUs">About Us</Link></button>
+                            <button className='w-[40%] bg-mainBg text-white font-medium pt-1 pb-1 rounded-2xl'> <Link to="/aboutUs">Find Doctor</Link></button>
                         </div>
                     </div> 
 
@@ -67,18 +68,18 @@ const Carousl = () => {
                 <div className=' flex flex-row justify-center items-center w-full h-full' >
                 <div className='w-[60%] h-full'>
                         <div className='p-4 md:p-20'>
-                            <h1 className='mb-3 md:mb-10 text-2xl md:text-6xl font-bold tracking-wider text-red-600 '>Search  Blood</h1>
+                            <h1 className='mb-3 md:mb-10 text-2xl md:text-6xl font-bold tracking-wider text-red-600 '>Find Blood</h1>
                             <p className=' text-sm md:text-2xl w-full'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem nesciunt consectetur aut aspernatur perferendis libero ipsam laboriosam et magnam eius blanditiis numquam iste, ad repellat tempore? Adipisci, ducimus id.</p>
                         </div>
                     
                         <div className=' ml-4 md:ml-20  w-[80%] md:w-[60%] flex flex-row justify-between text-sm md:text-2xl'>
-                            <button className='w-[40%] pt-1 pb-1 md:pt-4 md:pb-4 bg-mainBg text-white  font-semibold  rounded-sm'> <Link to="/aboutUs">Find Blood</Link></button>
-                            <button className='w-[50%] bg-mainBg text-white font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Donate Blood</Link></button>
+                            <button className='w-[40%] pt-1 pb-1 md:pt-4 md:pb-4 bg-mainBg text-white  font-medium  rounded-2xl'> <Link to="/aboutUs">Find Blood</Link></button>
+                            <button className='w-[50%] bg-mainBg text-white font-medium pt-1 pb-1 rounded-2xl'> <Link to="/aboutUs">Donate Blood</Link></button>
                         </div>
                 </div> 
 
                 <div className='w-[40%] h-full'>
-                    <img src={doc1} alt="" className='w-full h-full object-cover object-center' />
+                    <img src={blood} alt="" className='w-full h-full object-cover object-center' />
                 </div>
 
             
@@ -92,13 +93,13 @@ const Carousl = () => {
                         </div>
                         
                         <div className=' ml-4 md:ml-20  w-[80%] md:w-[80%] flex flex-row justify-between text-xs md:text-2xl'>
-                            <button className='w-[40%] md:pt-4 md:pb-4 bg-mainBg text-white  font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Find Ambulance</Link></button>
-                            <button className='w-[50%] bg-mainBg text-white font-semibold pt-1 pb-1 rounded-sm'> <Link to="/aboutUs">Register Ambulance</Link></button>
+                            <button className='w-[40%] rounded-2xl md:pt-4 md:pb-4 bg-mainBg text-white  font-medium pt-1 pb-1'> <Link to="/aboutUs">Find Ambulance</Link></button>
+                            <button className='w-[50%] bg-mainBg text-white font-medium pt-1 pb-1 rounded-2xl'> <Link to="/aboutUs">Register Ambulance</Link></button>
                         </div>
                     </div> 
 
                     <div className='w-[40%] h-full'>
-                        <img src={ambulance} alt="" className='w-full h-full object-cover object-center' />
+                        <img src={ambulance} alt="" className='w-full h-full object-fill object-center' />
                     </div>
 
             
